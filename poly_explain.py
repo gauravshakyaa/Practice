3044abed3b74d7037b8076131145ec1afb39a3f4
Gaurav <gauravshakya531@gmail.com>
2023-04-10 16:15:43 +0545

    Added login section

--- poly_explain.py
+++ poly_explain.py
@@ -1,6 +1,22 @@
-name = "Jim"  # str
+class Salary:
+    def __init__(self, pay, bonus):
+        self.pay = pay
+        self.bonus = bonus
 
-print(len(name))
+    def annual_salary(self):
+        return (self.pay * 12) + self.bonus
 
-some_list = ["some", "name"]
-print(len(some_list))
+
+class Employee:
+    def __init__(self, name, age, pay, bonus):
+        self.name = name
+        self.age = age
+        self.obj_salary = Salary(pay, bonus)
+
+    def total_salary(self):
+        return self.obj_salary.annual_salary()
+
+
+emp = Employee("max", 23, 15000, 10000)
+
+print(emp.total_salary())
--- seleniumWeb.py
+++ seleniumWeb.py
@@ -1,19 +1,30 @@
 import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 # os.environ['PATH'] += r"D:\Drivers\chromedriver_win32"
+class weblogin:
+    def __init__(self):
+        self.driver = webdriver.Chrome("D:\Drivers\chromedriver_win32\chromedriver.exe")
+        self.driver.maximize_window()
 
-driver = webdriver.Chrome(executable_path="D:\Drivers\chromedriver_win32\chromedriver.exe")
+    def login(self, num):
+        self.driver.get("https://web.karobarapp.com/login")  # Open the website
+        self.driver.implicitly_wait(3)  # Wait the website to load
 
-# driver = webdriver.Chrome()
+        assert self.driver.title == 'Login - Karobar', f'Expected Result: "Login - Karobar", Actual Result: {self.driver.title}'
 
-driver.get("web.karobarapp.com/login")  # Open the website
+        num_textbox_locator = self.driver.find_element(By.XPATH,
+                                                       '//input[@type="text" and @placeholder="Enter your Phone '
+                                                       'Number" and @class="input"]')
+        num_textbox_locator.send_keys(num)
+        time.sleep(4)
+        self.driver.quit()
 
-driver.implicitly_wait(3)  # Wait the website to load
 
-print(driver.title)
-
-assert driver.title == 'Yuwahub Community', f'Expected Result: "Yuwahub Community", Actual Result: {driver.title}'
+driver_karobar = weblogin()
+driver_karobar.login("9222222222")
